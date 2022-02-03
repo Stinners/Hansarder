bda083b9de615a5e2a693dc44d752f7b1f63ce16
Chris Stinson <chris.stinson202@gmail.com>
2022-02-03 20:22:13 +1300

    Improve db tests

--- db/functions/insert_document.py
+++ db/functions/insert_document.py
@@ -1,4 +1,4 @@
-from psycopg import Connection
+from psycopg import Connection, Cursor
 
 from typing import Optional, List
 
@@ -9,66 +9,63 @@ def get_query(name):
     with open("db/queries/" + name + ".sql", 'r') as f:
         return f.read()
 
-def insert_document_head(doc: HansardLink, conn: Connection) -> Optional[int]:
-    with conn.cursor() as curr:
-        curr = conn.cursor()
-        curr.execute (
-            get_query("insert_document"),
-            {
-                "title": doc.title,
-                "url": doc.url,
-                "start_date": doc.dates.actual_date,
-                "continued_date": doc.dates.continued_from,
-            }
-        )
-        fetch_id = curr.fetchone()
-        return fetch_id[0] if fetch_id is not None else None
+def insert_document_head(doc: HansardLink, cur: Cursor) -> Optional[int]:
+    cur.execute (
+        get_query("insert_document"),
+        {
+            "title": doc.title,
+            "url": doc.url,
+            "start_date": doc.dates.actual_date,
+            "continued_date": doc.dates.continued_from,
+        }
+    )
+    fetch_id = cur.fetchone()
+    return fetch_id[0] if fetch_id is not None else None
 
-def insert_debate_head(debate: DebateLink, doc_key: int, conn: Connection) -> Optional[int]:
-    with conn.cursor() as curr:
-        debate_values = {"title": debate.title, "debate_type": debate.type, "document_id": doc_key}
-        curr.execute(
-            get_query("insert_debate"),
-            debate_values
-        )
-        fetch_id = curr.fetchone()
-        return fetch_id[0] if fetch_id is not None else None
+def insert_debate_head(debate: DebateLink, doc_key: int, cur: Cursor) -> Optional[int]:
+    debate_values = {"title": debate.title, "debate_type": debate.type, "document_id": doc_key}
+    cur.execute(
+        get_query("insert_debate"),
+        debate_values
+    )
+    fetch_id = cur.fetchone()
+    return fetch_id[0] if fetch_id is not None else None
 
-def insert_speeches(speeches: List[SpeechLink], debate_key: int, conn: Connection):
-    with conn.cursor() as curr:
-        speech_mapping = [{
-            "topic": it.topic,
-            "member": it.speaker,
-            "html": it.html,
-            "speech_type": it.type,
-            "debate_id": debate_key,
-            "position": i,
-        } for (i, it) in enumerate(speeches)]
+def insert_speeches(speeches: List[SpeechLink], debate_key: int, cur: Cursor):
+    speech_mapping = [{
+        "topic": it.topic,
+        "member": it.speaker,
+        "html": it.html,
+        "speech_type": it.type,
+        "debate_id": debate_key,
+        "position": i,
+    } for (i, it) in enumerate(speeches)]
 
-        curr.executemany(
-            get_query("insert_speech"),
-            speech_mapping
-        )
+    cur.executemany(
+        get_query("insert_speech"),
+        speech_mapping
+    )
 
 
 
 def insert_document(conn, document):
-    try:
-        # This will throw and exception if try to insert a document that's alread in the database
-        doc_key = insert_document_head(document, conn)
-    #TODO find the appropriate exception class here
-    except:
-        return 
+    with conn.cursor() as cur:
+        try:
+            # This will throw and exception if try to insert a document that's alread in the database
+            doc_key = insert_document_head(document, cur)
+        #TODO find the appropriate exception class here
+        except:
+            return 
 
-    if doc_key == None:
-        raise Exception("Couldn't get key value from document")
+        if doc_key == None:
+            raise Exception("Couldn't get key value from document")
 
-    for debate in document.debates:
-        debate_key = insert_debate_head(debate, doc_key, conn)
-    
-        if debate_key == None:
-            raise Exception("Couldn't get key value from debate")
-   
-        insert_speeches(debate.speeches, debate_key, conn)
+        for debate in document.debates:
+                debate_key = insert_debate_head(debate, doc_key, cur)
+            
+                if debate_key == None:
+                    raise Exception("Couldn't get key value from debate")
+           
+                insert_speeches(debate.speeches, debate_key, cur)
 
-    conn.commit()
+        conn.commit()
--- tests/db/insert_document_test.py
+++ tests/db/insert_document_test.py
@@ -1,12 +1,20 @@
 
+import pytest
+import dotenv
+
 from datetime import date
 import random
 import string 
+import os
+import subprocess
 
 from ...scraper.scraper_types import HansardLink, SpeechLink, DebateLink, SessionDate
 from ...db.db import get_db
 from ...db.functions.insert_document import insert_document
 
+###################################################################
+#                          Data                                   #
+###################################################################
 
 test_speechs = [
     SpeechLink(
@@ -59,14 +67,63 @@ test_document = HansardLink(
     url = "www.test_url.com2" + random_id,
 )
 
-def get_query(name):
-    with open("db/queries/" + name + ".sql", 'r') as f:
-        return f.read()
+###################################################################
+#                         Fixtures                                #
+###################################################################
 
-# TODO: setup a proper testing database 
+@pytest.fixture(autouse=True)
+def set_env_test():
+    # Set the current environment to test 
+    dotenv.load_dotenv(".env_test")
 
-pool = get_db()
+    assert os.getenv("ENV") == "TEST", "Failed to set environment variable"
+
+    # Initiate the testing database, making sure it wasn't already there
+    subprocess.call(["dbmate", "-e", "TEST_DATABASE_URL", "drop"], stdout=subprocess.DEVNULL)
+    subprocess.call(["dbmate", "-e", "TEST_DATABASE_URL", "up"], stdout=subprocess.DEVNULL)
+
+    yield 
+
+    # Drop the test database after the test 
+    subprocess.call(["dbmate", "-e", "TEST_DATABASE_URL", "drop"], stdout=subprocess.DEVNULL)
+
+###################################################################
+#                         Tests                                   #
+###################################################################
 
 def test_insert_document():
+    pool = get_db()
+
     with pool.getconn() as conn:
         insert_document(conn, test_document)
+    
+        with conn.cursor() as cur:
+            cur.execute("""SELECT * FROM document""")
+            doc_id, title, url, actual_date, continued_date = cur.fetchone()   # type: ignore 
+
+            cur.execute("""SELECT * FROM debate ORDER BY id""")
+            debates = cur.fetchall()
+
+            cur.execute("""
+                SELECT s.id, s.html, m.name, p.name FROM speech s
+                LEFT JOIN member m ON m.id = s.member
+                LEFT JOIN party p ON m.party = p.id;
+            """)
+            speeches = cur.fetchall()
+
+            # Document Asserts 
+            assert url == test_document.url
+            assert title == test_document.title
+            assert actual_date == test_document.dates.actual_date
+            assert continued_date == test_document.dates.continued_from
+
+            # Debate Asserts 
+            assert len(debates) == 2 
+            assert debates[0][3] == debates[1][3] == doc_id
+            assert debates[0][1] == "Test Debate 1"
+
+            # Speech Asserts 
+            assert len(speeches) == 3
+            assert speeches[1][2] == 'Chlöe Swarbrick'
+            assert speeches[1][3] == "Green"
+
